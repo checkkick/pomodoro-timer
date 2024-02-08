cd2b49ad3fd72008826e0e2f4c43fc39daabf44d
Konstantin <tuskancik226@gmail.com>
2024-02-08 14:51:52 +0300

    create Main page component

--- src/routes/main.tsx
+++ src/routes/main.tsx
@@ -1,9 +1,10 @@
 import DefaultLayout from "@/layout/default"
+import { MainPage } from "@/pages/Main"
 
 export default function Main() {
    return (
       <DefaultLayout>
-         <div></div>
+         <MainPage />
       </DefaultLayout>
    )
 }
