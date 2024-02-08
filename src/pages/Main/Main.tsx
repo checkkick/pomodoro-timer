647326f9d3b33e8e904c40606aa1f68b1e95f802
Konstantin <tuskancik226@gmail.com>
2024-02-08 15:36:39 +0300

    add style for main text

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -2,32 +2,45 @@ import styles from './main.module.scss'
 
 export function MainPage() {
    return (
-      <div className={styles.columns}>
-         <div className={styles.tasks}>
-            <h2 className={styles.tasks__title}>Ура! Теперь можно начать работать:</h2>
+      <div className="container">
+         <div className={styles.columns}>
+            <div className={styles.tasks}>
+               <h2 className={styles.tasks__title}>Ура! Теперь можно начать работать:</h2>
 
-            <ul className={styles.tasks__list}>
-               <li className={styles.tasks__item}>
-                  <p className={styles.tasks__text}>Выберите категорию и напишите название текущей задачи</p>
-               </li>
-               <li className={styles.tasks__item}>
-                  <p className={styles.tasks__text}>Запустите таймер («помидор»)</p>
-               </li>
-               <li className={styles.tasks__item}>
-                  <p className={styles.tasks__text}>Работайте пока «помидор» не прозвонит</p>
-               </li>
-               <li className={styles.tasks__item}>
-                  <p className={styles.tasks__text}>Сделайте короткий перерыв (3-5 минут)</p>
-               </li>
-               <li className={styles.tasks__item}>
-                  <p className={styles.tasks__text}>Продолжайте работать «помидор» за «помидором», пока задача не будут выполнена. Каждые 4 «помидора» делайте длинный перерыв (15-30 минут).</p>
-               </li>
-            </ul>
+               <ul className={styles.tasks__list}>
+                  <li className={styles.tasks__item}>
+                     <p className={styles.tasks__text}>
+                        Выберите категорию и напишите название текущей задачи
+                     </p>
+                  </li>
+                  <li className={styles.tasks__item}>
+                     <p className={styles.tasks__text}>
+                        Запустите таймер («помидор»)
+                     </p>
+                  </li>
+                  <li className={styles.tasks__item}>
+                     <p className={styles.tasks__text}>
+                        Работайте пока «помидор» не прозвонит
+                     </p>
+                  </li>
+                  <li className={styles.tasks__item}>
+                     <p className={styles.tasks__text}>
+                        Сделайте короткий перерыв (3-5 минут)
+                     </p>
+                  </li>
+                  <li className={styles.tasks__item}>
+                     <p className={styles.tasks__text}>
+                        Продолжайте работать «помидор» за «помидором», пока задача не будут выполнена.
+                        Каждые 4 «помидора» делайте длинный перерыв (15-30 минут).
+                     </p>
+                  </li>
+               </ul>
 
-            {/* Форма с добавлением задач */}
+               {/* Форма с добавлением задач */}
+            </div>
+
+            {/* Форма с запуском таймера */}
          </div>
-
-         {/* Форма с запуском таймера */}
       </div>
    );
 }
