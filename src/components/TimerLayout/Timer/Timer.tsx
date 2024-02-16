4a2632c2edcaa4b59d2352eaa4160ea60ae991ae
Konstantin <tuskancik226@gmail.com>
2024-02-16 11:48:56 +0300

    add timer plus button

--- src/components/TimerLayout/Timer/Timer.tsx
+++ src/components/TimerLayout/Timer/Timer.tsx
@@ -1,10 +1,14 @@
 import { ButtonAccept } from '@/components/Forms/ButtonAccept';
 import styles from './timer.module.scss'
 import { ButtonDiscard } from '../../Forms/ButtonDiscard';
+import { TimerAddButton } from './TimerAddButton';
 
 export function Timer() {
    return (
       <div className={styles.block}>
+         <div className={styles.addBtn}>
+            <TimerAddButton />
+         </div>
          <p className={styles.watches}>26:00</p>
          <p className={styles.taskName}>
             <span className={styles.taskName__accent}>Задача 1 - </span> Сверстать сайт
